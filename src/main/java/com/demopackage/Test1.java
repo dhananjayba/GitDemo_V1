2a8e4d0091df384ed2c3f19a4c490e09c2827745
dhananjayba <bathedhananjay88@gmail.com>
2021-11-03 23:54:22 +0530

    first commit

---
+++ src/main/java/com/demopackage/Test1.java
@@ -0,0 +1,15 @@
+package com.demopackage;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		Boolean hungry;
+		if(hungry=true) {
+			System.out.println("Eat immediately");
+			
+		}else {
+			System.out.println("Do your work and complete task");
+		}
+	}
+
+}
